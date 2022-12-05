65b30f80597ef5a0f549b8a7cedcd2f4cea7dfe0
irshad007 <irshadmuhammedch8@gmail.com>
2022-12-05 17:27:01 +0530

    home

--- src/main/java/com/example/ProductApp/controller/productController.java
+++ src/main/java/com/example/ProductApp/controller/productController.java
@@ -1,8 +1,16 @@
 package com.example.ProductApp.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class productController {
+
+    @PostMapping("/")
+    public String home(){
+        return "welcome to product home page";
+
+    }
+
 
 }
